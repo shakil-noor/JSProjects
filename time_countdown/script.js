bae0da715d147109e28efd58d3661f9a77d1cffc
shakil-noor <hossain.bd1182@gmail.com>
2021-08-11 23:33:28 +0600

    JS Project - time count done

---
+++ time_countdown/script.js
@@ -0,0 +1,20 @@
+let timeH = document.querySelector('h1');
+let timeSecond = 11;
+
+//timeH.innerHTML = `00:${timeSecond}`;
+displayTime(timeSecond);
+
+const countDown = setInterval(()=>{
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond == 0){
+        timeH.innerHTML = 'Time out';
+        clearInterval(countDown);
+    }
+},1000);
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML = `${min<10 ? '0':''}${min}:${sec<10 ? '0': ''}${sec}`;
+}
